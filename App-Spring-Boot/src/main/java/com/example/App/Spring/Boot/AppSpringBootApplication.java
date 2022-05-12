cdf89331078ff15c8ceb2b20f7aea33c310c305b
kerly <kerlyvictoria21@gmail.com>
2022-05-12 13:05:17 +0200

    commit initial

---
+++ App-Spring-Boot/src/main/java/com/example/App/Spring/Boot/AppSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.App.Spring.Boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringBootApplication.class, args);
+	}
+
+}
---
+++ App-Spring-Boot/src/test/java/com/example/App/Spring/Boot/AppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.App.Spring.Boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
